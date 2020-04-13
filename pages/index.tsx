d016a2333317602803cc061928fd78f039027d0c
Chris Hutchinson <chris_hutchinson@me.com>
2020-04-13 18:02:26 +0100

    fix index title

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,14 @@ import Layout from "../components/Layout";
 const Index: NextPage = () => {
   return (
     <Layout>
+      <Head>
+        <title>An engineer's guide to...</title>
+        <meta
+          name="description"
+          content="Straightforward guides to core concepts for all software engineers"
+        />
+      </Head>
+
       <h2>Coming soon 🎉🎉</h2>
     </Layout>
   );
